1a225cab9ac58e70cd0c8231175333e8e4e922fb
mohandere <deremohan01@gmail.com>
2018-10-23 21:56:45 +0530

    modal

--- src/dashboard/Boards.js
+++ src/dashboard/Boards.js
@@ -1,27 +1,66 @@
 import React from 'react';
 import { boards } from './data';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import autoBind from 'auto-bind';
+import ReactModal from 'react-modal';
 
-const Boards = props => {
-  return (
-    <section className="page container">
-      <h1>Boards</h1>
-      <div id="boards">
-        {boards.map(b => (
-          <div key={b._id} className="board">
-            <Link
-              to={`${props.match.url}/b/${b._id}/${b.slug}`}
-              className="board-tile"
-            >
-              <div className="board-details">
-                <span className="board-title">{b.name}</span>
-              </div>
-            </Link>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
-};
+class Boards extends React.Component {
+  constructor(props) {
+    super(props);
+    autoBind(this);
+    this.el = document.createElement('div');
+    this.state = {
+      showModal: false
+    };
+  }
+
+  handleOpenModal() {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
+
+  render() {
+    let { match } = this.props;
+    let { showModal } = this.state;
+    return (
+      <section className="page container">
+        <h1>
+          Boards{' '}
+          <Button bsStyle="primary" onClick={this.handleOpenModal}>
+            + New Board
+          </Button>
+        </h1>
+        <div id="boards">
+          {boards.map(b => (
+            <div key={b._id} className="board">
+              <Link
+                to={`${match.url}/b/${b._id}/${b.slug}`}
+                className="board-tile"
+              >
+                <div className="board-details">
+                  <span className="board-title">{b.name}</span>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+
+        <ReactModal
+          isOpen={showModal}
+          className="Modal"
+          overlayClassName="Overlay"
+          contentLabel="Inline Styles Modal Example"
+        >
+          <p>Modal text!</p>
+          <button onClick={this.handleCloseModal}>Close Modal</button>
+        </ReactModal>
+      </section>
+    );
+  }
+}
 
 export default Boards;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { createStore } from 'redux';
 import trelloApp from './reducers';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ReactModal from 'react-modal';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
 // Create store
 const store = createStore(trelloApp);
 
+ReactModal.setAppElement('#root');
 render(<App store={store} />, document.getElementById('root'));
 
 registerServiceWorker();
